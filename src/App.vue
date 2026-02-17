f727ffd20e448b5f92fb3b0601105992c1976969
Mak3psh <109795770+Makspsh@users.noreply.github.com>
2026-02-17 13:08:59 +0300

    remove HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue';
+
 import ControlPanel from './components/ControlPanel.vue';
 import UserCamera from './components/UserCamera.vue';
 
@@ -8,15 +8,7 @@ const isCameraActive = ref(false);
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
   <ControlPanel
     :is-camera-active="isCameraActive"
     @toggle-camera="isCameraActive = !isCameraActive"
